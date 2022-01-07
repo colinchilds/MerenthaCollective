20f59186079a7683147bff693b930d8687306078
colinchilds <colinchilds@gmail.com>
2022-01-06 23:21:52 -0700

    Add grouping for special classes and keep dragons in sync

--- precache-manifest.ae095ceef1199b28421dcc59c2625fab.js
+++ precache-manifest.ae095ceef1199b28421dcc59c2625fab.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "897440b13866dd791a00484cb93f7528",
+    "revision": "64386d0b5805452389b34e7d4729e086",
     "url": "/index.html"
   },
   {
-    "revision": "ae241ac166f95d9c4354",
+    "revision": "3b0f06510a2657701219",
     "url": "/static/css/2.dc90a3ee.chunk.css"
   },
   {
-    "revision": "ae241ac166f95d9c4354",
-    "url": "/static/js/2.d5c73648.chunk.js"
+    "revision": "3b0f06510a2657701219",
+    "url": "/static/js/2.390bb8bb.chunk.js"
   },
   {
     "revision": "d0982c6ce8dcae4797ed059442dc24e4",
-    "url": "/static/js/2.d5c73648.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.390bb8bb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e96e84f6ee542e400923",
-    "url": "/static/js/main.44719823.chunk.js"
+    "revision": "4288c21163a2eff17288",
+    "url": "/static/js/main.313fef08.chunk.js"
   },
   {
     "revision": "dd2b242d605e30b8660e",
