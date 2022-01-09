a2c92ecea6963cd6ffe74f77ef2b37b4886d870f
colinchilds <colinchilds@gmail.com>
2022-01-09 14:33:10 -0700

    Add combat messages page

--- precache-manifest.dcf96c84643b2049f728b615c6f09f28.js
+++ precache-manifest.dcf96c84643b2049f728b615c6f09f28.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3124293b8f6b7d5f1f6af2dbb27ccf92",
+    "revision": "a47b66307a193940792805ab20bfddf3",
     "url": "/index.html"
   },
   {
-    "revision": "3fa58086cdf6d2287a62",
+    "revision": "5395b021cacaac2c1637",
     "url": "/static/css/2.dc90a3ee.chunk.css"
   },
   {
-    "revision": "3fa58086cdf6d2287a62",
-    "url": "/static/js/2.fde6e2a0.chunk.js"
+    "revision": "5395b021cacaac2c1637",
+    "url": "/static/js/2.739d4d24.chunk.js"
   },
   {
     "revision": "5feb0c30e35caf6e9faaee285f936112",
-    "url": "/static/js/2.fde6e2a0.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.739d4d24.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c4c1d1fc4e6fe0211735",
-    "url": "/static/js/main.d5824aa5.chunk.js"
+    "revision": "e5e9162da713b9503901",
+    "url": "/static/js/main.5aae2de5.chunk.js"
   },
   {
     "revision": "dd2b242d605e30b8660e",
