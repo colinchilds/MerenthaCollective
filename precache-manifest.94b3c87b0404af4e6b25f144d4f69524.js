cc9fcf657382a5b81f793a2d140684a5f65995d3
colinchilds <colinchilds@gmail.com>
2022-01-11 11:16:35 -0700

    Some more calculator styling tweeks. Added raw emote data for parsing

--- precache-manifest.94b3c87b0404af4e6b25f144d4f69524.js
+++ precache-manifest.94b3c87b0404af4e6b25f144d4f69524.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fe8a29a04b0514e912f676336062b077",
+    "revision": "25f059438874243243a4560982282b8e",
     "url": "/index.html"
   },
   {
-    "revision": "1cfd228de1804f9453c8",
+    "revision": "64c35df9a1af85433974",
     "url": "/static/css/2.dc90a3ee.chunk.css"
   },
   {
-    "revision": "1cfd228de1804f9453c8",
-    "url": "/static/js/2.8b4357f2.chunk.js"
+    "revision": "64c35df9a1af85433974",
+    "url": "/static/js/2.95da045b.chunk.js"
   },
   {
     "revision": "5feb0c30e35caf6e9faaee285f936112",
-    "url": "/static/js/2.8b4357f2.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.95da045b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3d03d5a7e6973cfbeddf",
-    "url": "/static/js/main.233657ac.chunk.js"
+    "revision": "67ff209d51f603cedc31",
+    "url": "/static/js/main.a306a60d.chunk.js"
   },
   {
     "revision": "dd2b242d605e30b8660e",
