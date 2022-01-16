e68415a03c095ad944b06688a52d4900a74eeba1
colinchilds <colinchilds@gmail.com>
2022-01-16 11:08:07 -0700

    Update more dependencies

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,7 +3,8 @@ module.exports = {
   bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
-  trailingComma: "all",
-  tabWidth: 2
+  trailingComma: "es5",
+  tabWidth: 2,
+  endOfLine: "auto"
   // Override any other rules you want
 };
