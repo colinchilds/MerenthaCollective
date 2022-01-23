461dad1e5149da94fd7a668275144e0a7db2a86e
Michael Angeli <michael.programs@gmail.com>
2022-01-23 16:53:34 -0500

    update build

---
+++ static/media/room.action.c5d4bf9245e5f2b3fc82.c
@@ -0,0 +1,23 @@
+#include <std.h>
+
+inherit ROOM;
+
+void create() {
+    ::create();
+    set_short("action room");
+    set_long("This is a room with an action: push button.");
+}
+
+void init() {
+    ::init();
+    add_action("cmd_push", "push");
+}
+
+int cmd_push(string input) {
+    if (!input || input != "button") {
+        write("Push what?");
+        return 1;
+    }
+    write("You push the button.");
+    say(this_player()->query_cap_name() + " pushes the button.");
+}
