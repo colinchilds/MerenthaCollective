ebe30cae890ac4ff6aae5d6f44442c3e233dcc43
jpgerber1986 <jordangerber@gmail.com>
2025-10-20 11:33:33 -0600

    Additional Data

--- src/data/Guides/index.js
+++ src/data/Guides/index.js
@@ -1,18 +1,40 @@
 export const areas = [
   {
     name: 'Summary',
-    data: [{ message: 'Summary goes here' }],
+    data: [
+      { message: 'Click on the area to the left to see the guide for the selected area.' },
+      { message: 'Content is approved by Admin but be sure to check out the MUD Rules.' },
+      { message: 'Check General > Areas for level specific areas.' },
+      { message: 'Check General > Maps for area related maps.' },
+    ],
   },
   {
     name: 'Cabeiri',
     data: [
-      { message: "Cabeiri is the newbie area. It's intended for players below level 10." },
-      { message: 'Check the General > Areas for level specifics.' },
+      { message: "Cabeiri is the newbie area. It's intended for players below ~level 10." },
       { message: 'There are x quests and y deeds in this area. See Rules if you have questions on these.' },
     ],
   },
   {
-    name: 'Area 52',
-    data: [{ message: 'Something' }, { message: 'Something Else' }],
+    name: 'Whitestorm',
+    data: [
+      { message: 'Whitestorm is the second area new exporers should check out.' },
+      { message: 'There are x quests and y deeds in this area. See Rules if you have questions on these.' },
+    ],
+  },
+  {
+    name: 'Fenris',
+    data: [
+      { message: 'Fenris includes the Forest of Deceoption, Marrdan, Trogrelin, Oz, and other areas.' },
+      { message: 'There are x quests and y deeds in this area. See Rules if you have questions on these.' },
+    ],
+  },
+  {
+    name: 'Wolvesdale',
+    data: [
+      { message: 'Wolvesdale is a very small area.' },
+      { message: 'WARNING: If you go in to the crypt and fight Velomuzst, he has the potential to hit for > 1000 hp.' },
+      { message: 'There are x quests and y deeds in this area. See Rules if you have questions on these.' },
+    ],
   },
 ];
