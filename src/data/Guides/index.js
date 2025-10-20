6fe6fe25c85abad8f6748a4d5ed96bdc1d0e9285
jpgerber1986 <jordangerber@gmail.com>
2025-10-20 11:18:21 -0600

    Began working on Area Guides data

---
+++ src/data/Guides/index.js
@@ -0,0 +1,18 @@
+export const areas = [
+  {
+    name: 'Summary',
+    data: [{ message: 'Summary goes here' }],
+  },
+  {
+    name: 'Cabeiri',
+    data: [
+      { message: "Cabeiri is the newbie area. It's intended for players below level 10." },
+      { message: 'Check the General > Areas for level specifics.' },
+      { message: 'There are x quests and y deeds in this area. See Rules if you have questions on these.' },
+    ],
+  },
+  {
+    name: 'Area 52',
+    data: [{ message: 'Something' }, { message: 'Something Else' }],
+  },
+];
