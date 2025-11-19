78089cb0d5199e01358c7b991ff95d617adb0017
jpgerber1986 <jordangerber@gmail.com>
2025-11-19 16:10:19 -0700

    Broke out data into data directory

--- src/routes/Pages/General/Combat.js
+++ src/routes/Pages/General/Combat.js
@@ -5,6 +5,8 @@ import Grid from '@mui/material/Grid';
 import CmtCard from '@coremat/CmtCard';
 import CmtCardContent from '@coremat/CmtCard/CmtCardContent';
 import { Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
+import Code from '@manannan/Code';
+import { MeleeCombat, SpellCombat } from 'data/Combat';
 
 const breadcrumbs = [
   { label: 'Main', link: '/' },
@@ -26,14 +28,21 @@ const Combat = () => {
         <Grid item xs={12}>
           <CmtCard>
             <CmtCardContent>
-              <Typography>
+              <Typography gutterBottom>
                 Below are two tables showing the message for each hit, for each of the three damage types. The percentage is
                 the damage compared to your enemy's maximum health points.
                 <br />
-                <br />
-                <b>Blunt:</b> Blunt, Melee, Flail, Projectile, Two handed
-                <br />
-                <b>Sharp:</b> Blade, Knife, Axe
+              </Typography>
+              <Typography gutterBottom>
+                <strong>Blunt:</strong> Blunt, Melee, Flail, Projectile, Two handed
+              </Typography>
+              <Typography gutterBottom>
+                <strong>Sharp:</strong> Blade, Knife, Axe
+              </Typography>
+              <Typography sx={{ fontStyle: 'italic' }}>
+                Some Spells/Abilities have their own output for the damage done and will repeat the same basic damage script,
+                which does not follow the tables below. E.g.: scorch always says <Code>hit</Code> but does more than 0%
+                damage.
               </Typography>
             </CmtCardContent>
             <CmtCardContent>
@@ -46,71 +55,13 @@ const Combat = () => {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  <TableRow>
-                    <TypographyCell>0%</TypographyCell>
-                    <TypographyCell>hit ineffectively</TypographyCell>
-                    <TypographyCell>hit ineffectively</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>0.0 - 0.7%</TypographyCell>
-                    <TypographyCell>tap innocently</TypographyCell>
-                    <TypographyCell>tap</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>0.7 - 1.4%</TypographyCell>
-                    <TypographyCell>tickle lightly</TypographyCell>
-                    <TypographyCell>tickle</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>1.4 - 2.0%</TypographyCell>
-                    <TypographyCell>sting</TypographyCell>
-                    <TypographyCell>sting sharply</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>2.0 - 2.7%</TypographyCell>
-                    <TypographyCell>hurt mildly</TypographyCell>
-                    <TypographyCell>graze</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>2.7 - 3.4%</TypographyCell>
-                    <TypographyCell>strike badly</TypographyCell>
-                    <TypographyCell>cut</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>3.4 - 4.7%</TypographyCell>
-                    <TypographyCell>strike</TypographyCell>
-                    <TypographyCell>slice</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>4.7 - 6.0%</TypographyCell>
-                    <TypographyCell>send a bone crushing blow</TypographyCell>
-                    <TypographyCell>slice horribly</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>6.0 - 7.4%</TypographyCell>
-                    <TypographyCell>knock</TypographyCell>
-                    <TypographyCell>shear to pieces</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>7.4 - 9.4%</TypographyCell>
-                    <TypographyCell>slam mercilessly</TypographyCell>
-                    <TypographyCell>strike with deadly force</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>9.4 - 11.4%</TypographyCell>
-                    <TypographyCell>blast powerfully</TypographyCell>
-                    <TypographyCell>mutilate</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>11.4 - 14.0%</TypographyCell>
-                    <TypographyCell>destroy</TypographyCell>
-                    <TypographyCell>dismember</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>14.0%+</TypographyCell>
-                    <TypographyCell>destroy utterly</TypographyCell>
-                    <TypographyCell>destroy utterly</TypographyCell>
-                  </TableRow>
+                  {MeleeCombat.map((row, i) => (
+                    <TableRow key={i}>
+                      <TypographyCell>{row.percent}</TypographyCell>
+                      <TypographyCell>{row.blunt}</TypographyCell>
+                      <TypographyCell>{row.sharp}</TypographyCell>
+                    </TableRow>
+                  ))}
                 </TableBody>
               </Table>
             </CmtCardContent>
@@ -123,58 +74,12 @@ const Combat = () => {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  <TableRow>
-                    <TypographyCell>0%</TypographyCell>
-                    <TypographyCell>hit ineffectively</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>0.0 - 1.4%</TypographyCell>
-                    <TypographyCell>tap innocently</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>1.4 - 2.0%</TypographyCell>
-                    <TypographyCell>tickle lightly</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>2.0 - 3.4%</TypographyCell>
-                    <TypographyCell>sting</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>3.4 - 4.0%</TypographyCell>
-                    <TypographyCell>hurt mildly</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>4.0 - 5.4%</TypographyCell>
-                    <TypographyCell>strike badly</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>5.4 - 6.7%</TypographyCell>
-                    <TypographyCell>strike</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>6.7 - 8.7%</TypographyCell>
-                    <TypographyCell>send a bone crushing blow</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>8.7 - 10.7%</TypographyCell>
-                    <TypographyCell>knock</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>10.7 - 14.0%</TypographyCell>
-                    <TypographyCell>slam mercilessly</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>14.0 - 16.7%</TypographyCell>
-                    <TypographyCell>blast powerfully</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>16.7 - 20.7%</TypographyCell>
-                    <TypographyCell>destroy</TypographyCell>
-                  </TableRow>
-                  <TableRow>
-                    <TypographyCell>20.7%+</TypographyCell>
-                    <TypographyCell>destroy utterly</TypographyCell>
-                  </TableRow>
+                  {SpellCombat.map((row, index) => (
+                    <TableRow key={index}>
+                      <TypographyCell>{row.percent}</TypographyCell>
+                      <TypographyCell>{row.magic}</TypographyCell>
+                    </TableRow>
+                  ))}
                 </TableBody>
               </Table>
             </CmtCardContent>
