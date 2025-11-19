a6cd8a1b9dfce931d28b9ed9ac325b51551b8311
jpgerber1986 <jordangerber@gmail.com>
2025-11-19 14:26:29 -0700

    Updated with new component <Code>

--- src/routes/Pages/Components/InfoRequests.js
+++ src/routes/Pages/Components/InfoRequests.js
@@ -1,20 +1,22 @@
 import * as React from 'react';
 import CmtCard from '@coremat/CmtCard';
 import CmtCardContent from '@coremat/CmtCard/CmtCardContent';
+import Code from '@manannan/Code';
 import { Link, Typography } from '@mui/material';
 
 const InfoRequests = () => {
   return (
     <CmtCard id="issuesCell">
       <CmtCardContent>
-        <Typography align={'center'}>
+        <Typography align="center" gutterBottom>
           If you would like to add or update any information found in the zone details, please submit a request on{' '}
-          <Link href="https://github.com/colinchilds/MerenthaCollective/issues" target="_blank">
+          <Link href="https://github.com/colinchilds/MerenthaCollective/issues" target="_blank" rel="noopener noreferrer">
             GitHub Issues
           </Link>
         </Typography>
-        <Typography align={'center'}>
-          Use the label <strong>PLACEHOLDER</strong>.
+
+        <Typography align="center">
+          Use the label <Code>PLACEHOLDER</Code>
         </Typography>
       </CmtCardContent>
     </CmtCard>
