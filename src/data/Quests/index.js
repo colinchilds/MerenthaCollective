3e9cf28d394852e7334078a4a1c41c896219f947
jpgerber1986 <jordangerber@gmail.com>
2025-12-10 22:55:42 -0700

    Placeholder quest data

--- src/data/Quests/index.js
+++ src/data/Quests/index.js
@@ -1,6 +1,23 @@
 const questData = [
   {
-    //
+    name: 'Test Name',
+    description: 'Some test quest goes here!',
+    level: 20,
+  },
+  {
+    name: 'Test Name 2',
+    description: 'Some test quest goes here!',
+    level: 20,
+  },
+  {
+    name: 'Test Name 3',
+    description: 'Some test quest goes here!',
+    level: 20,
+  },
+  {
+    name: 'Test Name 4',
+    description: 'Some test quest goes here!',
+    level: 20,
   },
 ];
 
