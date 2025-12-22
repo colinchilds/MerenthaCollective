1603ec1527d47f4a55e463537703b0369eb43f4b
Colin Childs <colinchilds@gmail.com>
2025-12-22 11:37:44 -0700

    Revert to eslint 8, since 9 is not compatible with our setup

--- eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
